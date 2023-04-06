467292c867500018d98e4407491d272462b16524
suyog <suyogshirsath11@gmail.com>
2023-04-06 18:49:24 +0530

    PageableResponse and ImageResponse completed

---
+++ src/main/java/com/bikkadit/electronicstore/dtos/PageableResponse.java
@@ -0,0 +1,18 @@
+package com.bikkadit.electronicstore.dtos;
+
+import lombok.*;
+
+import java.util.List;
+
+@Getter @Setter
+@AllArgsConstructor @NoArgsConstructor
+@Builder
+public class PageableResponse<T> {
+
+    private List<T> content;
+    private int pageNumber;
+    private int pageSize;
+    private long totalElement;
+    private int totalPages;
+    private boolean lastPage;
+}
