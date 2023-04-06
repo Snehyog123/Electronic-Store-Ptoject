79200eff48070a968676a03aff7750f15b99bbf8
suyog <suyogshirsath11@gmail.com>
2023-04-06 18:50:10 +0530

    AppConstants and AppResponse created

---
+++ src/main/java/com/bikkadit/electronicstore/helper/AppConstants.java
@@ -0,0 +1,16 @@
+package com.bikkadit.electronicstore.helper;
+
+public class AppConstants {
+
+    public static final String RESOURCE_DELETED_SUCCESS="Resource deleted successfully !!!";
+
+    public static final String YES="Y";
+
+    public static final String NO="N";
+
+    public static final String NOT_ACTIVE="User is soft Deleted from Database";
+
+//    public static final String INVALID_USERNAME="Username is invalid , Character must in-between 3-15 !!!";
+
+
+}
