fe0aa89ee9b3e9dc45cdaa31a3c823f3c9cc9acf
suyog <suyogshirsath11@gmail.com>
2023-04-06 18:41:27 +0530

    User Repository created

---
+++ src/main/java/com/bikkadit/electronicstore/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package com.bikkadit.electronicstore.repository;
+
+
+import com.bikkadit.electronicstore.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByEmailAndPassword(String email, String password);
+
+    List<User> findByNameContaining(String keyword);
+
+}
